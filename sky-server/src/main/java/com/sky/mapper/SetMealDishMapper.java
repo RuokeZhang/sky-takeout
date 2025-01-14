d9bf65d332af8a489f012266b314d07e5ccdb67a
RuokeZhang <zhang.12783@osu.edu>
2025-01-14 20:47:36 +0800

    completed adding setmeal

--- sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetMealDishMapper.java
@@ -1,12 +1,17 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface SetMealDishMapper {
-    public List<Long> getSetMealDishIdListByDishIds(List<Long> dishIds);
     //select setmeal_id from setmeal_dish where dish_id in (1,2,3,4)
+    public List<Long> getSetMealDishIdListByDishIds(List<Long> dishIds);
+
+
+    void batchSave(List<SetmealDish> dishList);
+
 
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,6 +1,11 @@
 package com.sky.mapper;
 
+import com.sky.annotation.Autofill;
+import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -13,5 +18,16 @@ public interface SetmealMapper {
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
+
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("INSERT INTO setmeal ("
+            + " category_id, name, price, status, description, image, "
+            + " update_time, update_user, create_time, create_user"
+            + ") VALUES ("
+            + " #{categoryId}, #{name}, #{price}, #{status}, #{description}, #{image}, "
+            + " #{updateTime}, #{updateUser}, #{createTime}, #{createUser}"
+            + ")")
+    @Autofill(value= OperationType.INSERT)
+    void save(Setmeal setmeal);
 
 }
